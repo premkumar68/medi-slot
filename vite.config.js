0f033c89ef09c7ed3104c595e11fd5ca9cc2226d
Prem kumar <surya5167088@gmail.com>
2025-07-22 15:31:21 +0530

    added vit.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/', // 👈 Add this line
   plugins: [react(), tailwindcss()],
 })
